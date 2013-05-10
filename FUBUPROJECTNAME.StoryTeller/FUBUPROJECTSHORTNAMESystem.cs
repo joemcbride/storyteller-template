75d7cf4f5ae4a36dc0dc5db7da6ccfcddc348176
Joe McBride <joe@uicraftsman.com>
2013-05-10 16:32:02 -0600

    Initial commit

---
+++ FUBUPROJECTNAME.StoryTeller/FUBUPROJECTSHORTNAMESystem.cs
@@ -0,0 +1,20 @@
+﻿using StoryTeller.Engine;
+
+namespace FUBUPROJECTNAME.StoryTeller
+{
+    public class FUBUPROJECTSHORTNAMESystem : ISystem
+    {
+        public void Dispose()
+        {
+        }
+
+        public IExecutionContext CreateContext()
+        {
+            return new SimpleExecutionContext();
+        }
+
+        public void Recycle()
+        {
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+# What is this?
+
+This is a ripple-enabled StoryTeller project template for fubu new.
+
+# Usage
+
+      fubu new <Solution Name> -g git://github.com/DarthFubuMVC/rippletemplate.git
+      cd <Solution Name>
+      fubu new <Project Name> -g git://github.com/joemcbride/storyteller-template.git -s src/<Solution Name>.sln
